35e20540b2cbf0fcb8ea14a94783590109ca09f0
dressrahmat <dedysrahmat97@gmail.com>
2024-03-13 08:48:03 +0700

    Video Ke 5 Selesai Prepare The Project

---
+++ app/Models/Task.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'status',
+        'priority',
+        'deadline',
+    ];
+
+    /**
+     * Get the user that owns the Task
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
