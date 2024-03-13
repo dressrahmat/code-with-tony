67f51d1f1a502cbb8a874a6cc451c72d7e9db482
dressrahmat <dedysrahmat97@gmail.com>
2024-03-13 10:23:29 +0700

    video ke tujuh selesai (create task)

--- app/Livewire/Tasks/TasksIndex.php
+++ app/Livewire/Tasks/TasksIndex.php
@@ -6,6 +6,28 @@ use Livewire\Component;
 
 class TasksIndex extends Component
 {
+    public $title;
+    public $slug;
+    public $description;
+    public $status;
+    public $priority;
+    public $deadline;
+
+    public function save()
+    {
+        // $this->validate([
+        //     'title' => 'required',
+        //     'slug' => 'required',
+        //     'description' => 'required',
+        //     'status' => 'required',
+        //     'priority' => 'required',
+        //     'deadline' => 'required',
+        // ]);
+
+        auth()->user()->tasks()->create($this->only(['title', 'slug', 'description', 'status', 'priority', 'deadline']));
+        $this->reset();
+    }
+
     public function render()
     {
         return view('livewire.tasks.tasks-index')->layout('layouts.app');
--- resources/views/livewire/tasks/tasks-index.blade.php
+++ resources/views/livewire/tasks/tasks-index.blade.php
@@ -1,32 +1,52 @@
 <div class="flex justify-center items-center mt-5">
-    <form class="w-1/3">
+    <form wire:submit="save" class="w-1/3">
+        <!-- Title -->
         <div class="mb-3">
-            <label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-            <input type="text" name="" id="" class="w-full h-12 rounded-md">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+            <input type="text" wire:model="title" id="title" class="w-full h-12 rounded-md">
         </div>
+
+        <!-- Slug -->
         <div class="mb-3">
-            <label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
-            <input type="text" name="" id="" class="w-full h-12 rounded-md">
+            <label for="slug" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Slug</label>
+            <input type="text" wire:model="slug" id="slug" class="w-full h-12 rounded-md">
         </div>
+
+        <!-- Description -->
         <div class="mb-3">
-            <label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-            <textarea type="text" name="" id="" class="w-full rounded-md" rows="3"></textarea>
+            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+            <textarea type="text" wire:model="description" id="description" class="w-full rounded-md" rows="3"></textarea>
         </div>
+
+        <!-- Status -->
         <div class="mb-3">
-            <label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
-            <select name="" id="" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500">
+            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+            <select wire:model="status" id="status" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500">
                 @foreach (\App\Enums\StatusType::cases() as $status)
                 <option value="{{ $status->value }}">{{ $status->name }}</option>
                 @endforeach
             </select>
         </div>
+
+        <!-- Priority -->
         <div class="mb-3">
-            <label for="" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Priority</label>
-            <select name="" id="" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500">
+            <label for="priority" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Priority</label>
+            <select wire:model="priority" id="priority" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500">
                 @foreach (\App\Enums\PriorityType::cases() as $status)
                 <option value="{{ $status->value }}">{{ $status->name }}</option>
                 @endforeach
             </select>
+        </div>
+
+        <!-- Deadline -->
+        <div class="mb-3">
+            <label for="deadline" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deadline</label>
+            <input type="date" wire:model="deadline" id="" class="w-full h-12 rounded-md">
+        </div>
+
+        <!-- Deadline -->
+        <div class="mb-3">
+            <button type="submit" class="bg-indigo-500 p-2 rounded-lg w-full text-white">Save</button>
         </div>
 
     </form>
