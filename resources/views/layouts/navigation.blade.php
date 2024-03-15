e82267da382e2b55fb4db206ebcb3e2f8e08babe
dressrahmat <dedysrahmat97@gmail.com>
2024-03-15 10:00:16 +0700

    video ke 17 selesai (live search input)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,6 +19,9 @@
                         {{ __('Tasks') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    @livewire('search')
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
